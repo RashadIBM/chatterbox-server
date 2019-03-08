902dfb394131c96aaa60d04ab266e0520703b238
NickRashad <nrsternes@gmail.com>
2019-03-08 01:32:17 -0600

    Reflect server changes in browser w nodemon & Fix require requestHandler

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,34 +1,67 @@
 /* Import node's http module: */
 var http = require('http');
-var handleRequest = require('./basic-server.js');
+var { requestHandler } = require('./request-handler.js');
+var handleRequest = requestHandler;
+
+var port = 3000;
+var ip = '127.0.0.1';
+
+var server = http.createServer(handleRequest);
+console.log('Listening on http://' + ip + ':' + port);
+server.listen(port, ip);
+
+
+// http.createServer((request, response) => {
+//   const { headers, method, url } = request;
+//   console.log('url:', url, 'method', method, 'headers:', headers);
+//   let body = [];
+//   request.on('error', (err) => {
+//     console.error(err);
+//   }).on('data', (chunk) => {
+//     body.push(chunk);
+//   }).on('end', () => {
+//     body = Buffer.concat(body).toString();
+//     console.log(body);
+//     response.on('error', (err) => {
+//       console.error(err);
+//     });
+
+//     response.statusCode = 200;
+//     response.setHeader('Content-Type', 'application/json');
+//     // Note: the 2 lines above could be replaced with this next one:
+//     // response.writeHead(200, {'Content-Type': 'application/json'})
+//     body = { message: 'Hi'};
+//     const responseBody = { headers, method, url, body };
+
+//     response.write(JSON.stringify(responseBody));
+//     response.end();
+//     // At this point, we have the headers, method, url and body, and can now
+//     // do whatever we need to in order to respond to this request.
+//   });
+// }).listen(port, ip);
+
+
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
 // normally already claimed by another server and/or not accessible
 // so we'll use a standard testing port like 3000, other common development
 // ports are 8080 and 1337.
-var port = 3000;
 
 // For now, since you're running this server on your local machine,
 // we'll have it listen on the IP address 127.0.0.1, which is a
 // special address that always refers to localhost.
-var ip = '127.0.0.1';
-
-
 
 // We use node's http module to create a server.
 //
 // The function we pass to http.createServer will be used to handle all
 // incoming requests.
 //
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
-console.log('Listening on http://' + ip + ':' + port);
-server.listen(port, ip);
+// After creating the server, we will tell it to listen on the given port and IP.
 
 // To start this server, run:
 //
-//   node basic-server.js
+//   node server/basic-server.js
 //
 // on the command line.
 //
@@ -38,4 +71,3 @@ server.listen(port, ip);
 // server.listen() will continue running as long as there is the
 // possibility of serving more requests. To stop your server, hit
 // Ctrl-C on the command line.
-
